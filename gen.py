f50fcdbdf3f6eac5bc5a1de58ac11ba231b59a89
davidjnovak <51279385+davidjnovak@users.noreply.github.com>
2023-06-03 01:02:15 -0400

    Adding logic to find file contents and issue description

--- gen.py
+++ gen.py
@@ -2,23 +2,46 @@ import sys
 import os
 import openai
 import requests
-import bs4
+from bs4 import BeautifulSoup
+import re
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Use the following instructions: " + issue_description + " to generate a README file in markdown given the following repository: " + repository
-  temperature=0.7,
-  max_tokens=979,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-  )
-
 issue_url = 'https://github.com/Jonathan-Zollinger/eDirectory-Compose/issues/1'
+pattern = r'(https://github.com/[^/]+/[^/]+)/.*'
+match = re.search(pattern, issue_url)
+
+if match:
+    repository_url = match.group(1)
+    print("Repository URL:", repository_url)
+else:
+    print("Invalid GitHub issue URL")
 response = requests.get(issue_url)
-soup = bs4(response.content, 'lxml')
+soup = BeautifulSoup(response.content, 'lxml')
 issue_description = soup.find(class_='markdown-body').get_text()
+# Define the GitHub file URL
+url = 'https://github.com/Jonathan-Zollinger/eDirectory-Compose/blob/main/src/eDir_utils.sh'
+print(url)
+response = requests.get(url)
+
+# Parse the HTML content of the response
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
+# Find all the code blocks in the file content
+fileContents = ""
+code_blocks = soup.find_all('div', class_='highlight')
+for block in code_blocks:
+    if block.find('span', {'class': 'nf'}) and block.find('span', {'class': 'kd'}) == 'def':
+        fileContents += code_block.find('pre').text
 
 
-print(issue_description)
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt="Use the following instructions: " + issue_description + " to generate a README file in markdown given the following file: " + fileContents,
+#   temperature=0.7,
+#   max_tokens=979,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+#   )
+
+print("Use the following instructions: " + issue_description + " to generate a README file in markdown given the following file: " + fileContents)
